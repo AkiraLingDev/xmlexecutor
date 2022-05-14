b95a1232c1207e3f88650bcfb83b017acfc29d6a
Akira <afelia.master@gmail.com>
2022-05-14 11:20:12 +0300

    Форма для проверки карты сайта на главной

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php require_once($_SERVER["DOCUMENT_ROOT"]."/core/prolog.php");
 
 //echo "Hello world";
+?>
+<div class="xml-input-main-block">
+    <div class="xml-input-main-label">Введите адрес сайта для проверки:</div>
+    <div class="xml-input-main-input">
+        <input type="text" name="xml-input-main"> <button id="xml-main-button">Проверить</button>
+    </div>
+</div>
